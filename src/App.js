327d497647f7064a40072e68f70d1a3ebf6aece0
bhvskiran <82263450+bhvskiran@users.noreply.github.com>
2022-01-09 20:45:41 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, This is Sai Kiran's World
+          Hello, I am  Sai Kiran
         </p>
+        <p>This is my World</p>
         <a
           className="App-link"
           href="https://reactjs.org"
