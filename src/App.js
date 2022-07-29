933887cb22f65d3368edb9ad85a0a614f6aa5774
bhvskiran <bhvskiran@gmail.com>
2022-07-29 21:55:39 +0530

    added prettier and eslint in project

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, I am  Sai Kiran
-        </p>
-        <p>This is SK's World</p>
+        <p>Hello, I am Sai Kiran</p>
+        <p>{`This is SK's World`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
